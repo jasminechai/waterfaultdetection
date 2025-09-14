f68bc60a268d6fa3c0a796fcb0c98e358ed1e85a
Jasmine <90776806+jasminechai@users.noreply.github.com>
2025-09-14 16:21:25 -0400

    Add files

---
+++ setup.py
@@ -0,0 +1,12 @@
+# from dev
+
+from setuptools import find_packages, setup
+
+setup(
+    name='Jasmine',
+    version=0.1,
+    author='jasmine',
+    author_email="jasminec4894@gmail.com",
+    packages=find_packages(), # goes in folder and finds all packages
+install_requires = []
+)
