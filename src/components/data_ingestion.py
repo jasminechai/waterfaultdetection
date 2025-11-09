df697bab41f91d92cdfa633f23f1e8cddf27b286
Jasmine <90776806+jasminechai@users.noreply.github.com>
2025-11-09 05:37:40 -0500

    Your descriptive commit message

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,1 +1,88 @@
- 
+import sys, os
+import numpy as np
+import pandas as pd
+from pymongo import MongoClient # 
+from zipfile import Path # unzipping files
+from src.constant import *
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import MainUtils
+from dataclasses import dataclass
+
+# decorator
+@dataclass
+Class DataIngestionConfig:
+    # storing path so other functions can access it
+    artifact_folder: str = os.path.join("artifacts")
+
+
+class DataIngestion:
+    def __init__(self):
+        self.data_ingestion_config = DataIngestionConfig()
+        self.MainUtils = MainUtils()
+    
+    def export_collection_as_dataframe(self, collection_name, db_name):
+        try:
+            mongo_client = MongoClient(MONGO_DB_URL)
+            collection = mongo_client[db_name][collection_name]
+            df = pd.DataFrame(list(collection.find()))
+            if '_id' in df.columns.to_list():
+                df = df.drop(columns=['_id'], axis = 1)
+
+            df.replace({'na': np.nan}, inplace = True)
+            return df
+        except Exception as e:
+            raise CustomException(e, sys)
+    
+    def export_data_into_feature_store_file_path(self) -> pd.DataFrame:
+        """
+        Method Name: export_data_into_feature_store
+        Description: This method reads data from mongodb and saves it into artifacts.
+
+        Output     : dataset is returned as a pd.DataFrame
+        On Failure : Write an exception log and then raise an exception
+
+        Version    : 1.0
+        """
+        try:
+            logging.info(f"Exporting data from mongodb")
+            raw_file_path = self.data_ingestion_config.artifact_folder
+            os.makedirs(raw_file_path, exist_ok=True)
+
+            sensor_data = self.export_collection_as_dataframe(
+                collection_name=MONGO_COLLECTION_NAME,
+                db_name=MONGO_DATABASE_NAME
+                )
+            
+            logging.info(f"saving exported data into feature store file path: (raw_file)")
+
+            feature_store_file_path = os.path.join(raw_file_path, 'water_fault.csv')
+            sensor_data.to_csv(feature_store_file_path, index = False)
+
+            return feature_store_file_path
+
+        except Exception as e:
+            raise CustomException(e, sys) from e
+
+    def initiate_data_ingestion(self) -> Path:
+        """
+        Method Name: initiate_data_ingestion
+        Description: This method initiates the data ingestion components of training pipeline
+
+        Output     : train set and test set are returned as the artifacts of data ingestion compenents
+        On Failure : Write an exception log and then raise an exception
+
+        Version    : 1.0
+        """
+        logging.info("Entered initiate_data_ingestion method of Data_Ingestion class")
+
+        try:
+            feature_store_file_path = self.export_data_into_feature_store_file_path()
+
+            logging.info(
+                "Exited initiate_data_ingestion method of Data_ingestion class"
+            )
+            return feature_store_file_path
+
+        except Exception as e:
+            raise CustomException(e, sys) from e
--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,1 +1,96 @@
- 
+import sys, os
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import RobustScaler, FunctionTransformer, StandardScaler
+from sklearn.pipeline import Pipeline
+
+from src.constant import *
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import MainUtils
+from dataclasses import dataclass
+
+@dataclass
+class DataTransformationConfig:
+    artifact_dir = os.path.join(artifact_folder)
+    transformed_test_file_path = os.path.join(artifact_dir, "test.npy")
+    transformed_train_file_path = os.path.join(artifact_dir, "train.npy")
+    transformed_object_file_path = os.path.join(artifact_dir, "preprocessing.pkl")
+
+class DataTransformation:
+    def __init__(self, feature_store_file_path):
+        self.feature_store_file_path = feature_store_file_path
+        self.DataTransformationConfig = DataTransformationConfig()
+
+        self.utils = MainUtils()
+
+    @staticmethod
+    def get_data(feature_store_file_path: str) -> pd.DataFrame:
+        """
+        Method Name: get_data
+        Description: This method reads all the validated raw data from the feature_store_file_path and returns a par
+
+        Output     : a pandas DataFrame containing the merged data
+        On Failure : Write an exception log and then raise an exception
+
+        Version    : 1.0
+        """
+        try:
+            data = pd.read_csv(feature_store_file_path)
+            data.rename(columns={'Good/Bad': TARGET_COLUMN}, inplace = True)
+            return data
+
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+    def get_data_transformer_object(self):
+        # preprocess and pipeline
+        try:
+            preprocessor = Pipeline(
+                steps=[
+                    ('imputer', SimpleImputer(strategy='constant', fill_value=0)),
+                    ('scaler', RobustScaler())
+                ]
+            )
+            return preprocessor
+
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+    def initiate_data_transformation(self):
+        """
+        Method Name: initiate_data_transformation
+        Description: This method initiates the data transformation component for the pipeline.
+
+        Output     : data transformation artifact is created and restarted
+        On Failure : Write an exception log and then raise an exception
+
+        Version    : 1.0
+        """
+        try:
+            dataframe = self.get_data(feature_store_file_path=self.feature_store_file_path)
+
+            X = dataframe.drop(columns=TARGET_COLUMN)
+            y = np.where(dataframe[TARGET_COLUMN] == -1, 0, 1)
+
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+
+            preprocessor = self.get_data_transformer_object()
+            X_train_scaled = preprocessor.fit_transform(X_train)
+            X_test_scaled = preprocessor.transform(X_test)
+
+            preprocessor_path = self.DataTransformationConfig.transformed_object_file_path
+            os.makedirs(os.path.dirname(preprocessor_path, exist_ok=True))
+            self.utils.save_object(file_path=preprocessor_path, obj=preprocessor)
+
+            train_arr = np.c_[X_train_scaled, np.array(y_train)]
+            test_arr = np.c_[X_test_scaled, np.array(y_test)]
+
+            return (train_arr, test_arr, preprocessor_path)
+        
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+    
